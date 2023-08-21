fa4658130bb16bb176be87aacd9f56439894992e
adnanshaz <135756041+adnanshaz@users.noreply.github.com>
2023-08-21 11:58:40 +0500

    Add files via upload

---
+++ 13TH ASSIGNMENT/app.js
@@ -0,0 +1,90 @@
+// <<<<<<< JS assignment CHP 13-15 >>>>>>////
+
+// Q NO 1 //
+
+// var student = [];
+
+// Q no 2 // 
+
+// var student = [];
+
+// Q no 3 //
+
+// var str = ["mohammad","adnan","ali","umer"]
+ 
+// Q no 4 //
+
+// var num = [123456,030300,032426,908980];
+
+// Q no 5 //
+
+// var boole = [true,false];
+
+// Q no 6 //
+
+// var mixed = ["Adnan",030300,true,"umer"]
+
+// Q no 7 //
+
+// var qualification = [
+// SSC
+// HSC
+// BCS
+// BS
+// B.COM
+// M.S
+// M.PHIL
+// Phd
+// ];
+
+// document.write("<h2>Qualifications:</h2>");
+// document.write(qualifictions);
+
+// Q No 8 //
+
+
+// var students = ["Adnan", "Mohammad", "umer"];
+// var score = [430, 270, 500];
+// var totalMarks = 1200;
+
+// document.write(
+//   "Score of " +
+//     students[0] +
+//     " is " +
+//     score[0] +
+//     ". Percentage: " +
+//     (score[0] / 700) * 100 +
+//     "%"
+// );
+
+// document.write(
+//   "<br/> Score of " +
+//     students[1] +
+//     " is " +
+//     score[1] +
+//     ". Percentage: " +
+//     (score[1] / 800) * 100 +
+//     "%"
+// );
+
+// document.write(
+//   "<br/>Score of " +
+//     students[2] +
+//     " is " +
+//     score[2] +
+//     ". Percentage: " +
+//     (score[2] / 900) * 100 +
+//     "%"
+// );
+
+// Q No 9 //
+
+
+
+//Questin No-10
+
+
+
+//Questin No-11
+
+//<<<<------>>>>>////
